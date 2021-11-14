8ba34b9204c48545d350d2d90679cf3421639594
nxtcoder17 <nxtcoder17@gmail.com>
2021-11-14 18:25:47 +0530

    jsconfig paths issue fixed

--- app/controller.js
+++ app/controller.js
@@ -1,4 +1,5 @@
 import fsPromise from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 
 const controller = {};
@@ -15,11 +16,11 @@ controller.exists = async (item) => {
 const packageJson = async () => {
   try {
     const content = await fsPromise.readFile(`${process.cwd()}/package.json`);
-    const { alias } = JSON.parse(content.toString());
-    if (!alias) {
+    const data = JSON.parse(content.toString());
+    if (!data.alias) {
       process.exit(0);
     }
-    return alias;
+    return data;
   } catch (err) {
     // console.log(err.message);
     return process.exit(17);
@@ -27,50 +28,45 @@ const packageJson = async () => {
 };
 
 controller.linkAlias = async () => {
-  const alias = await packageJson();
+  const { alias } = await packageJson();
 
   const p = Object.keys(alias).map(async (item) => {
     const sourcePath = path.resolve(process.cwd(), alias[item]);
     const targetPath = path.resolve(process.cwd(), 'node_modules', item);
     const relativePath = path.relative(path.dirname(targetPath), sourcePath);
-
     await fsPromise.symlink(relativePath, targetPath);
-
-    const jsconfigPath = `${process.cwd()}/jsconfig.json`;
-
-    if (await fsPromise.exists(jsconfigPath)) {
-      const pathAliases = Object.entries(packageJson.alias).reduce(
-        (acc, [k, v]) => {
-          if (fsPromise.lstatSync(v).isDirectory()) {
-            return { ...acc, [`${k}/*`]: [`${v}/*`] };
-          }
-          return {
-            ...acc,
-            [k]: v,
-          };
-        },
-        {}
-      );
-
-      const content = await fsPromise.readFile(jsconfigPath);
-      const jsconfig = JSON.parse(content.toString());
-      jsconfig.compilerOptions = jsconfig.compilerOptions || {};
-      jsconfig.compilerOptions.paths = {
-        ...(jsconfig.compilerOptions.paths || {}),
-        ...pathAliases,
-      };
-      await fsPromise.writeFile(
-        jsconfigPath,
-        JSON.stringify(jsconfig, null, 2)
-      );
-    }
   });
+
+  const jsconfigPath = `${process.cwd()}/jsconfig.json`;
+
+  if (await controller.exists(jsconfigPath)) {
+    const pathAliases = Object.entries(alias).reduce((acc, [k, v]) => {
+      const stat = fs.lstatSync(v);
+      if (stat.isDirectory()) {
+        return { ...acc, [`${k}/*`]: [`${v}/*`] };
+      }
+      return {
+        ...acc,
+        [k]: [v],
+      };
+    }, {});
+
+    const content = await fsPromise.readFile(jsconfigPath);
+    const jsconfig = JSON.parse(content.toString());
+    jsconfig.compilerOptions = jsconfig.compilerOptions || {};
+    jsconfig.compilerOptions.paths = {
+      ...(jsconfig.compilerOptions.paths || {}),
+      ...pathAliases,
+    };
+
+    await fsPromise.writeFile(jsconfigPath, JSON.stringify(jsconfig, null, 2));
+  }
 
   return Promise.all(p);
 };
 
 controller.unlinkAlias = async () => {
-  const alias = await packageJson();
+  const { alias } = await packageJson();
 
   const p = Object.keys(alias).map(async (item) => {
     const targetPath = path.resolve(process.cwd(), 'node_modules', item);
